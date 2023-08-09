04b907427e0fb6b90be48d57216c09589144678a
Thomas Albuquerque <tealbthomas@gmail.com>
2023-08-09 09:23:56 -0300

    Working version 1.0.0.

---
+++ src/index.ts
@@ -0,0 +1,54 @@
+import fs from 'fs'
+import axios from 'axios'
+import { AxiosResponse } from 'axios'
+
+interface Post {
+  postId: string,
+  id: string,
+  name: string,
+  email: string,
+  body: string,
+}
+
+async function fetchData() {
+  const result: AxiosResponse = await axios.get('https://jsonplaceholder.typicode.com/comments')
+  const posts: Post[] = result.data
+  const formattedPosts = posts.map(post => (`From: ${post.email}
+${post.name} say ${post.body}`)
+  )
+  return formattedPosts
+}
+
+function writeFile(posts: string[]) {
+  if (fs.existsSync('outputs/output.txt')) {
+    fs.unlink('outputs/output.txt', function (error) {
+      if (error) throw error;
+      console.log('Old file deleted.')
+    });
+  }
+
+  let i = 50
+  const iterations = setInterval(() => {
+    console.log(i)
+    const piece = JSON.stringify(posts.slice(i - 50, i), null, 2);
+    const output = piece.slice(1, piece.length - 2) + ','
+    fs.appendFile('outputs/output.txt', output, 'utf8', (error) => {
+      if (error) {
+        console.error('File writing failed.', error)
+        return
+      }
+      console.log('File writing succeeded.')
+    })
+    i = i + 50
+    if (i > posts.length) {
+      clearInterval(iterations)
+    }
+  }, 1000)
+}
+
+async function run() {
+  const posts = await fetchData()
+  writeFile(posts)
+}
+
+await run()
